2a87fce034710f5933937e8dcaa9a22e720334fb
udeh princewill <princezubby2020@gmail.com>
2021-09-02 14:11:06 +0100

    display name fix

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,10 @@
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
-import React,{forwardRef} from 'react'
 
-const Thumbnail = forwardRef(({ result }, ref) => {
+const Thumbnail = ({ result }) => {
     const BASE_URL = 'https://image.tmdb.org/t/p/original/'
     return (
-        <div ref={ref} className='group cursor-pointer p-4 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50' >
+        <div className='group cursor-pointer p-4 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50' >
             <Image 
                 layout='responsive'
                 src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || 
@@ -26,6 +25,6 @@ const Thumbnail = forwardRef(({ result }, ref) => {
             </div>
         </div>
     )
-})
+}
 
 export default Thumbnail
